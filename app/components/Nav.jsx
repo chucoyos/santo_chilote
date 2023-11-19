6de7478b9696ec263b5d8f33c5fa272296703c7b
chucoyos <unodev@outlook.com>
2023-11-19 01:11:30 -0600

    Add mobile menu-icon

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -2,13 +2,15 @@ import Link from 'next/link'
 import Image from 'next/image'
 const Nav = () => {
 	return (
-		<nav className='nav flex items-center justify-between border border-gray-300'>
+		<nav className='px-5 flex nav items-center justify-between border border-gray-300'>
 			<Link
 				href='/'
 				className=''
 			>
-				<div className='flex flex-col items-center'>
-					<p className='text-orange-900 text-center italic'>Fish Tacos</p>
+				<div className='flex flex-col items-center '>
+					<p className='hidden lg:block text-orange-900 text-center italic'>
+						Fish Tacos
+					</p>
 					<Image
 						src='/logo.jpeg'
 						alt='Santo Chilote logo'
@@ -16,10 +18,12 @@ const Nav = () => {
 						height={40}
 						className='rounded-full'
 					/>
-					<p className='text-orange-900 text-center italic'>Santo Chilote</p>
+					<p className='hidden lg:block text-orange-900 text-center italic'>
+						Santo Chilote
+					</p>
 				</div>
 			</Link>
-			<div className='flex items-center text-orange-900'>
+			<div className='hidden lg:block flex items-center text-orange-900'>
 				<Link
 					href='/menu'
 					className='mr-4'
@@ -27,6 +31,22 @@ const Nav = () => {
 					Menu
 				</Link>
 				<a href='/about'>About</a>
+			</div>
+			<div className='lg:hidden md:block flex text-orange-900'>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					fill='none'
+					viewBox='0 0 24 24'
+					strokeWidth={1.5}
+					stroke='currentColor'
+					className='w-10 h-10'
+				>
+					<path
+						strokeLinecap='round'
+						strokeLinejoin='round'
+						d='M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5'
+					/>
+				</svg>
 			</div>
 		</nav>
 	)
--- app/page.js
+++ app/page.js
@@ -4,8 +4,10 @@ export default function Home() {
 	return (
 		<div className='flex min-h-screen flex-col items-center justify-between'>
 			<div className='flex flex-col items-center hero'>
-				<h1 className='text-8xl font-bold text-white'>Santo Chilote</h1>
-				<p className='text-xl text-white'>
+				<h1 className='text-4xl lg:text-6xl font-bold text-white mb-4'>
+					Santo Chilote
+				</h1>
+				<p className='text-xl text-white text-center'>
 					Best Mexican fish tacos in the Todos Santos area
 				</p>
 			</div>
